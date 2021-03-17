5dbfcbd3af31828f5f28f1d81f111a6d15fd483b
bagantonyan <bag.antonyan@gmail.com>
2021-03-18 00:07:58 +0400

    Add project files.

---
+++ Tictactoe.App/MainForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Tictactoe.App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 133);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(133, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 133);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(266, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 133);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(266, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 133);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            this.button6.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button6.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(133, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 133);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            this.button5.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button5.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 133);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button9
+            // 
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(266, 266);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 133);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            this.button9.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button9.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(133, 266);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 133);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            this.button8.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button8.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(0, 266);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 133);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            this.button7.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            this.button7.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(261, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(11, 375);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(128, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(11, 375);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 261);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(375, 11);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 11);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Image = ((System.Drawing.Image)(resources.GetObject("exitButton.Image")));
+            this.exitButton.Location = new System.Drawing.Point(312, 513);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 75);
+            this.exitButton.TabIndex = 14;
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.FlatAppearance.BorderSize = 0;
+            this.newGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newGameButton.Image = ((System.Drawing.Image)(resources.GetObject("newGameButton.Image")));
+            this.newGameButton.Location = new System.Drawing.Point(12, 513);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(75, 75);
+            this.newGameButton.TabIndex = 15;
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(399, 600);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button newGameButton;
+    }
+}
+
---
+++ Tictactoe.App/MainForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tictactoe.App
+{
+    public partial class MainForm : Form
+    {
+        private bool checkTurn;
+        private Button[,] buttons;
+        public MainForm()
+        {
+            InitializeComponent();
+            checkTurn = false;
+            buttons = new Button[3, 3]
+            {
+                { button1, button2, button3 },
+                { button4, button5, button6 },
+                { button7, button8, button9 },
+            };
+        }
+
+        private void button_MouseEnter(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Enabled)
+            {
+                if (checkTurn)
+                {
+                    button.Image = Properties.Resources.tac;
+                }
+                else
+                {
+                    button.Image = Properties.Resources.tic;
+                }
+            }
+        }
+
+        private void button_MouseLeave(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Enabled)
+            {
+                button.Image = null;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (checkTurn)
+            {
+                button.Image = Properties.Resources.tac;
+            }
+            else
+            {
+                button.Image = Properties.Resources.tic;
+            }
+            checkTurn = !checkTurn;
+            button.Enabled = false;
+            FindWinner(buttons, true);
+            FindWinner(buttons, false);
+        }
+
+        public void FindWinner(Button[,] buttons, bool alignment)
+        {
+            bool hasWinner = false;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                hasWinner = true;
+                for (int j = 1; j < buttons.GetLength(1); j++)
+                {
+                    if (alignment)
+                    {
+                        if (buttons[i, 0].Enabled || buttons[i, j].Enabled
+                         || buttons[i, 0].Image.Size != buttons[i, j].Image.Size)
+                        {
+                            hasWinner = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (buttons[0, i].Enabled || buttons[j, i].Enabled
+                         || buttons[0, i].Image.Size != buttons[j, i].Image.Size)
+                        {
+                            hasWinner = false;
+                            break;
+                        }
+                    }
+                }
+                ShowWinner(hasWinner);
+            }
+
+            for (int i = 1; i < buttons.GetLength(0); i++)
+            {
+                hasWinner = true;
+                if (alignment)
+                {
+                    if (buttons[0, 0].Enabled || buttons[i, i].Enabled
+                     || buttons[0, 0].Image.Size != buttons[i, i].Image.Size)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (buttons[0, buttons.GetLength(1) - 1].Enabled || buttons[i, buttons.GetLength(1) - 1 - i].Enabled
+                     || buttons[0, buttons.GetLength(1) - 1].Image.Size != buttons[i, buttons.GetLength(1) - 1 - i].Image.Size)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+            }
+            ShowWinner(hasWinner);
+        }
+
+        public void ShowWinner(bool hasWinner)
+        {
+            if (hasWinner)
+            {
+                string winner = "";
+                if (checkTurn)
+                {
+                    winner = "X";
+                }
+                else
+                {
+                    winner = "O";
+                }
+
+                MessageBox.Show("Winner is -" + winner);
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void NewGame()
+        {
+            foreach (var button in buttons)
+            {
+                button.Image = null;
+                button.Enabled = true;
+                checkTurn = false;
+            }
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            foreach (var button in buttons)
+            {
+                button.Image = null;
+                button.Enabled = true;
+                checkTurn = false;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
